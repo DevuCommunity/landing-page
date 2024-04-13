9d0db931f126dd3a0f0dce3d0c857748eee7263a
Jose Maria CLi <chema9703@gmail.com>
2024-04-13 01:48:26 -0600

    feat: add draft sections with tailwind

---
+++ src/modules/landing/components/layout/language-selector.tsx
@@ -0,0 +1,15 @@
+
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+type Language = 'MX' | 'EN' | 'NAHUATL'
+
+interface LanguageSelectorProps {
+  currentLanguage: Language
+}
+
+export const LanguageSelector = ({
+  currentLanguage = 'MX'
+}: LanguageSelectorProps) => {
+  return (
+    <span className="fi fis fi-mx" />
+  )
+}
